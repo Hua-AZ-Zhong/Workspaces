1b84696f86f8eb31d14bf4b2fbdfef8917180063
Hua-AZ-Zhong <253729119@qq.com>
2019-04-04 00:15:03 +0800

    update

--- MyEclipse_8.6/IBM/src/demo/Divide.java
+++ MyEclipse_8.6/IBM/src/demo/Divide.java
@@ -2,20 +2,20 @@ package demo;
 
 public class Divide {
 
-	public Divide() {
-		// TODO Auto-generated constructor stub
-	}
+    public Divide() {
+        // TODO Auto-generated constructor stub
+    }
 
-	/**
-	 * @param args
-	 */
-	static final float PI = 3.14f;
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int x = 10;
-		int y = 3;
-		System.out.println(x / y);
-		System.out.println(PI);
-	}
+    /**
+     * @param args
+     */
+    static final float PI = 3.14f;
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int x = 10;
+        int y = 3;
+        System.out.println(x / y);
+        System.out.println(PI);
+    }
 
 }
