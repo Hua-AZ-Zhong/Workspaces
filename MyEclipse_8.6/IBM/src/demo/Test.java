5713e964623abf4de60cc18f4a427c771044646d
Hua-AZ-Zhong <253729119@qq.com>
2019-04-09 11:33:55 +0800

    replace tabspace

--- MyEclipse_8.6/IBM/src/demo/Test.java
+++ MyEclipse_8.6/IBM/src/demo/Test.java
@@ -3,19 +3,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Test
 {
-	public static void main(String args[]) {
-		System.out.println("hello world");
-		int number = 0; 
-		try {
-			System.out.print("please input number of line:");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-	        number = Integer.valueOf(reader.readLine());
-		} catch (IOException ex) {   
-        }   
-		Star star=new Star();
-		star.printStar(number);
-	}
+    public static void main(String args[]) {
+        System.out.println("hello world");
+        int number = 0;
+        try {
+            System.out.print("please input number of line:");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            number = Integer.valueOf(reader.readLine());
+        } catch (IOException ex) {
+        }
+        Star star=new Star();
+        star.printStar(number);
+    }
 }
